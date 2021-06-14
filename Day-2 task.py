75f4bc5fe4209fa3bcb54141a4e213b251d89ed0
Akilan10 <61017057+Akilan10@users.noreply.github.com>
2021-06-14 22:06:17 +0530

    Add files via upload

---
+++ Day-2 task.py
@@ -0,0 +1,50 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("30 days 30 hours challenge")
+30 days 30 hours challenge
+>>> print('30 days 30 hours challenge')
+30 days 30 hours challenge
+>>> hours="thirty"
+>>> print(hours)
+thirty
+>>> days="thirty days"
+>>> print(days[2])
+i
+>>> name="Akilan"
+>>> print(name[0])
+A
+\
+>>> challenge="Lets do it"
+>>> print(challenge[7:10])
+ it
+>>> challenge="Lets do it"
+>>> print(len(challenge))
+10
+>>> challenge="lets do it"
+>>> print(challenge.lower())
+lets do it
+>>> challenge="LETS DO IT"
+>>> print(challenge.lower())
+lets do it
+>>> a="30 days"
+>>> b="30 hours"
+>>> c=a+""+b
+>>> print(c)
+30 days30 hours
+>>> text="thirty days thirty hours"
+>>> x=text.casefold()
+>>> print(x)
+thirty days thirty hours
+>>> text="thirty days thirty hours"
+>>> x=text.capitalize()
+>>> print(x)
+Thirty days thirty hours
+>>> text="thirty days thirty hours"
+>>> x=text.isalpha()
+>>> print(x)
+False
+>>> text="thirty days thirty hours"
+>>> x=text.isalnum()
+>>> print(x)
+False
+>>> 
