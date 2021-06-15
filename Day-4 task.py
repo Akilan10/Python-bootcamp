fe673e0d1b3a5c74ea9c38480dac4d721fbf1fd7
Akilan10 <61017057+Akilan10@users.noreply.github.com>
2021-06-15 13:50:39 +0530

    Add files via upload

---
+++ Day-4 task.py
@@ -0,0 +1,21 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=b=c=100
+>>> print("dividing a by 10:",a/10)
+dividing a by 10: 10.0
+>>> print("multiplying b by 50:",b*50)
+multiplying b by 50: 5000
+>>> print("add c by value 60:",c+60)
+add c by value 60: 160
+>>> name="gojou"
+>>> name
+'gojou'
+>>> print("after replacing G string :",name[0:2]+'G'+name[3:5])
+after replacing G string : goGou
+>>> a=10
+>>> b=69.69
+>>> print("After typecasting a to float:",float(a))
+After typecasting a to float: 10.0
+>>> print("Afer typecasting b to int:",int(b))
+Afer typecasting b to int: 69
+>>> 
