70a2bbbe6f9b86e9dd53fa396cac5748c7bc5bf4
Akilan10 <61017057+Akilan10@users.noreply.github.com>
2021-06-27 21:48:01 +0530

    Add files via upload

---
+++ Day-12 task.py
@@ -0,0 +1,7 @@
+  
+f = open('30 days 30 hour operations', 'a')
+f.write('I have completed 10 days successfully ')
+f.write('by Akilan.S')
+f = open('30 days 30 hour operations', 'r')
+print(f.read())
+f.close()
